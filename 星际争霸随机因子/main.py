cb1e8135c0b7b2353286ac703a8f428a49a41237
Burn-Sin <Burn_Sin@live.com>
2021-08-29 18:02:05 +0800

    重装后的提交

--- 星际争霸随机因子/main.py
+++ 星际争霸随机因子/main.py
@@ -1,10 +1,10 @@
-import re,random
+import re,random,os
 
 map = ['机会渺茫','泯灭快车','虚空降临','克哈裂痕','虚空撕裂','升格之链','聚铁成兵','净网行动','黑暗杀星','熔火危机','营救矿工','王者之夜','天界封锁','死亡摇篮','往日神庙']
 
 regex = r"(?<=\d\.\s).*?(?=\：)"
 
-yinzi = open('F:\my_python_code\星际争霸随机因子\因子.txt',encoding='utf-8')
+
 
 while True:
 
@@ -12,8 +12,16 @@ while True:
 
     num = input('')
 
+
+
+    path = os.getcwd() + '\因子.txt'
+
+    print(path)
+
+    yinzi = open(path,encoding='utf-8')
+
     yinzi_list = re.findall(regex, yinzi.read())
 
     print(random.sample(yinzi_list, int(num)))
 
-    num = input('')
+    na = input('')
